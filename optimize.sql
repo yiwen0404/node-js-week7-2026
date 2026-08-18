7732804a6610587fa42a24af13bc7ad45b203108
yiwen Chen <yiwen850404@gmail.com>
2026-08-18 12:19:46 +0800

    yiwen deliver : node-js-week7-2026

--- optimize.sql
+++ optimize.sql
@@ -11,19 +11,26 @@
 -- ============================================================
 
 -- 工單 1：客服查會員
-
+CREATE INDEX idx_users_email
+ON users (email);
 
 -- 工單 2：企業會員的課表
-
+CREATE INDEX idx_course_bookings_practice
+ON course_bookings (user_id, cancelled_at);
 
 -- 工單 3：最新購買紀錄牆
-
+CREATE INDEX idx_credit_purchases_purchase_at
+ON credit_purchases (purchase_at);
 
 -- 工單 4：首頁「進行中課程」
-
+CREATE INDEX idx_courses_end_at
+ON courses (end_at);
 
 -- 工單 5：上週開課課程的教練報名統計（思考方向：需新增兩個索引）
+CREATE INDEX idx_courses_start_at ON courses (start_at);
 
+CREATE INDEX idx_course_bookings_course_id
+ON course_bookings (course_id);
 
 -- 加分題（選做）：使用部分索引（partial index）讓工單 2 的索引更小、更有效率
 
